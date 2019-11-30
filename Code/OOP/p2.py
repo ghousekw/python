4ec7449cbaa92942c7e8fff7a2ae5f03833fe4a7
gmshaik <gmshaik.kw@gmail.com>
2019-11-30 20:53:33 +0300

    Using class variables

--- Code/OOP/p2.py
+++ Code/OOP/p2.py
@@ -1,29 +1,34 @@
 class Employee:
+    raise_amount = 1.04
+    num_of_emp = 0
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + "." + last + "@email.com"
 
+        Employee.num_of_emp += 1
+
     def full_name(self):
         return f"{self.first} {self.last}"
 
     def apply_raise(self):
-        self.pay = self.pay * 1.04
+        self.pay = int(self.pay * self.raise_amount)
 
+# Before Instantiated Class Employee
+print(Employee.num_of_emp)
 
 emp1 = Employee("Areeb", "Shaik", 20000)
 emp2 = Employee("Test", "user", 30000)
 
-print(emp1.email)
-
-#   printing by using instance
-print(emp1.full_name())
-print(emp2.full_name())
-
-# Printing by class
-print(Employee.full_name(emp1))
-
-print(emp1.pay)
-emp1.apply_raise()
-print(emp1.pay)
+# After Instantiated Class Employee
+print(Employee.num_of_emp)
+# print(emp1.__dict__)
+# # print(Employee.__dict__)
+# print(Employee.raise_amount)  # prints the class variable value
+# # Changing emp1 raise_amount
+# emp1.raise_amount = 1.08
+# print(emp1.__dict__)
+# print(emp1.raise_amount)
+# print(emp2.raise_amount)
