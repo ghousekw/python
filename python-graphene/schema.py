4188867391e8eb77abb8a3d54ba1369dc4965441
ghousekw <gmshaik.kw@gmail.com>
2020-01-26 08:46:09 +0300

    Cmds used, about graphene

---
+++ python-graphene/schema.py
@@ -0,0 +1,25 @@
+import graphene
+import json
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+    is_admin = graphene.Boolean()
+
+    def resolve_hello(self, info):
+        return "world"
+    
+    def resolve_is_admin(sef, info):
+        return True
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute(
+    '''
+    {
+        isAdmin
+    }
+    '''
+)
+
+dictResult = dict(result.data.items())
+print(json.dumps(dictResult, indent=2))
